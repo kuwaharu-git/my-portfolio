d2e50f484b265f1fa0383fc2cb44593ea520cadb
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2024-11-19 00:02:24 +0900

    add Index component and integrate Header

---
+++ src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../components/Header';
+
+const Index = () => {
+    return (
+        <div>
+            <Header />
+            {/* Other components or content can go here */}
+        </div>
+    );
+};
+
+export default Index;
