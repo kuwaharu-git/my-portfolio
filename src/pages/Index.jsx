a6ea91a6599d236b73f2ae762312b87ab3d87199
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2024-11-19 21:42:33 +0900

    refactor Index component to include Top and Profile components with Footer

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Header } from '../components/Header';
+import Header from '../components/Header';
+import { Footer } from '../components/Footer';
+import { Top } from '../components/Index/Top';
+import { Profile } from '../components/Index/Profile';
 
 const Index = () => {
     return (
         <div>
             <Header />
-            {/* Other components or content can go here */}
+            <main>
+                <Top />
+                <Profile />
+            </main>
+            <Footer />
         </div>
     );
 };
