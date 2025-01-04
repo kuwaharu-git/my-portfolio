34be740610c29bb432438fe15e1780d9c82f7b9e
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-01-04 18:36:15 +0900

    資格コンポーネントに合格情報と関連リンクを追加

---
+++ src/components/Skill/qualifications.jsx
@@ -0,0 +1,56 @@
+import { Description, DescriptionUl } from "../Card";
+
+
+export const qualifications = {
+    "ITパスポート": {"description": (<Description>
+        独学で勉強し2023年3月に合格<br />
+        結果詳細
+        <DescriptionUl>
+            <li>総合評価点: 755点</li>
+            <li>ストラテジ系: 670点</li>
+            <li>マネジメント系: 735点</li>
+            <li>テクノロジ系: 780点</li>
+        </DescriptionUl>
+    </Description>)},
+    "基本情報技術者試験": {"description": (<Description>
+        2023年12月に合格<br />
+        結果詳細
+        <DescriptionUl>
+            <li>科目A: 800点</li>
+            <li>科目B: 865点</li>
+        </DescriptionUl>
+        関連記事
+        <DescriptionUl>
+            <li><a href="https://note.com/kuwaharu/n/n8100cf34a64a">基本情報技術者試験に合格しました(note)</a></li>
+        </DescriptionUl>
+    </Description>)},
+    "応用情報技術者試験": {"description": (<Description>
+        2024年7月に合格<br />
+        結果詳細
+        <DescriptionUl>
+            <li>午前: 66.25点</li>
+            <li>午後: 67点</li>
+        </DescriptionUl>
+        関連記事
+        <DescriptionUl>
+            <li><a href="https://note.com/kuwaharu/n/n91900d3dcff8">応用情報技術者試験受験報告(note)</a></li>
+            <li><a href="https://note.com/kuwaharu/n/n89d259c14fc7">応用情報技術者試験に合格しました(note)</a></li>
+        </DescriptionUl>
+    </Description>)},
+    "情報処理安全確保支援士試験": {"description": (<Description>
+        2024年12月に合格<br />
+        結果詳細
+        <DescriptionUl>
+            <li>午前Ⅰ: 免除</li>
+            <li>午前Ⅱ: 72点</li>
+            <li>午後: 61点</li>
+        </DescriptionUl>
+        関連記事
+        <DescriptionUl>
+            <li><a href="https://note.com/kuwaharu/n/nfb23b38c2785">情報処理安全確保支援士試験受験報告(note)</a></li>
+            <li><a href="https://note.com/kuwaharu/n/n7cd3e8d12c9f">情報処理安全確保支援士試験合格報告(note)</a></li>
+        </DescriptionUl>
+    </Description>)},
+};
+
+
