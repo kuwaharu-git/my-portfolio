4c937819e7adaf8d86132c0bfde92562134dbf2c
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-02-07 19:15:51 +0900

    skillページのレイアウトの修正 close #9

--- src/components/Skill/skillCommonStyles.js
+++ src/components/Skill/skillCommonStyles.js
@@ -5,20 +5,28 @@ import breakpoints from '../../breakpoints';
 export const TitleWrapper = styled.h1`
     box-sizing: border-box;
     width: 100%;
+    margin: 0 auto;
     text-align: left;
     font-size: 40px;
     font-weight: bold;
     color: #000000;
     margin: 30px 0;
+    text-align: center;
     @media (max-width: ${breakpoints.mobile}) {
       font-size: 30px;
     }
 `;
 
-export const CardsWrapper = styled.ul`
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-    list-style: none;
-    padding: 0;
+export const CardsListWrapper = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 284px);
+  gap: 40px;
+  list-style: none;
+  padding: 0;
+  justify-content: space-evenly; // 左右均等配置
+  max-width: 1400px;
+  margin: 0 auto;
+  @media (max-width: ${breakpoints.mobile}) {
+    grid-template-columns: repeat(auto-fit, 284px);
+  }
 `;
