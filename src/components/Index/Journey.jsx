ed780f4139b3b74db94033eaf543fda7cea8fd46
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-02-11 20:10:29 +0900

    タイムラインデータをjourneyData.jsに移動し、Journeyコンポーネントでインポート

--- src/components/Index/Journey.jsx
+++ src/components/Index/Journey.jsx
@@ -2,21 +2,8 @@
 import styled from '@emotion/styled'
 import breakpoints from '../../breakpoints'; // ブレイクポイントをインポート
 import { motion } from "framer-motion";
+import { timelineData } from './journeyData'; // タイムラインデータをインポート
 
-// タイムラインデータ
-const timelineData = [
-  { date: '2022-11', content: 'Javascriptを初めて触る' },
-  { date: '2023-3', content: 'ITパスポート合格' },
-  { date: '2023-4', content: '専門学校入学\nPython, MySQLを初めて触る' },
-  { date: '2023-6', content: 'paiza Bランク獲得' },
-  { date: '2023-9', content: 'Javaを初めて触る' },
-  { date: '2023-12', content: '基本情報技術者試験合格' },
-  { date: '2024-4', content: 'ドロカツ全国大会\n自律飛行スキルクラス優勝' },
-  { date: '2024-5', content: 'paiza Aランク獲得' },
-  { date: '2024-7', content: '応用情報技術者試験合格' },
-  { date: '2024-9', content: '自動運転AIチャレンジ予選敗退' },
-  { date: '2024-12', content: '情報処理安全確保支援士試験合格' },
-]
 
 // メインコンポーネント
 export const Journey = () => {
